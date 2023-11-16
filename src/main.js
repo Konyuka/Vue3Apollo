12a434ddbc4bc27be354ac9b2ab07223675843a9
Konyuka <michaelsaiba84@gmail.com>
2023-11-16 16:58:45 +0300

    apollo setup and query test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-const app = createApp(App)
+const httpLink = createHttpLink({
+    uri: 'http://localhost:8000/graphql',
+})
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
 
 app.use(router)
-
 app.mount('#app')
